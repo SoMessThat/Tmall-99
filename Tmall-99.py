c750a8d73119a5243a2007c501c948c7dd5c520b
wukaibin <842387850@qq.com>
2019-10-30 17:00:01 +0800

    python可执行文件。 修改坐标适应任务位置被调整

--- Tmall-99.py
+++ Tmall-99.py
@@ -2,7 +2,7 @@
 # coding: utf8
 # author: Mess
 # createTime: 2019-09-02 16:58:56
-# updateTime: 2019-10-22 10:52:11
+# updateTime: 2019-10-30 15:49:16
 
 import os
 import time
@@ -19,10 +19,11 @@ def goback():
     os.system('.\\adb.exe shell input keyevent KEYCODE_BACK')  # 返回
 
 
-def task1():
+def shopping():
+    #多两次避免已逛过该店
     for i in range(1, 21):
-        print('===== 逛*****店({}/20) ======'.format(i))
-        os.system('.\\adb.exe shell input tap 900 1200')   # 进入逛店
+        print('===== 逛*****店({}/18) ======'.format(i))
+        os.system('.\\adb.exe shell input tap 900 1620')   # 进入逛店
         initlocal()
         print('进入店铺，浏览中，请等待 16 秒')
         time.sleep(16)
@@ -30,37 +31,36 @@ def task1():
         time.sleep(1)
     print('已完成逛店任务')
     
-def task2():
-    print('====== 浏览会场 ======')
-    for i in range(1, 4):
-        os.system('.\\adb.exe shell input tap 900 1350')   # 点击去浏览
-        initlocal()
-        print('进入会场，浏览中，请等待 16 秒')
-        time.sleep(16)
-        goback()
-    print('已完成浏览会场任务')
+def task1():
+    print('====== 浏览主会场 ======')
+    os.system('.\\adb.exe shell input tap 900 1150')   # 点击去浏览
+    initlocal()
+    print('进入主会场，浏览中，请等待 16 秒')
+    time.sleep(16)
+    goback()
+    print('已完成浏览主会场任务')
     print('====== 浏览其他会场 ======')
     for i in range(1, 4):
-        os.system('.\\adb.exe shell input tap 900 1600')   # 点击去浏览1
+        os.system('.\\adb.exe shell input tap 900 1300')   # 点击去浏览1
         time.sleep(2)   # 滑动前休息2秒，避免网络不好
         initlocal()
         print('进入会场，浏览中，请等待 16 秒')
         time.sleep(16)
         goback()
 
-def task3():
+def task2():
     print('====== 浏览会场 ======')
-    for i in range(1, 3):
-        os.system('.\\adb.exe shell input tap 900 1400')   # 点击去浏览
-        time.sleep(2)   # 滑动前休息2秒，避免网络不好
-        initlocal()
-        print('进入会场，浏览中，请等待 16 秒')
-        time.sleep(16)
-        goback()
+    # for i in range(1, 4):
+    os.system('.\\adb.exe shell input tap 900 1500')   # 点击去浏览
+    time.sleep(2)   # 滑动前休息2秒，避免网络不好
+    initlocal()
+    print('进入会场，浏览中，请等待 16 秒')
+    time.sleep(16)
+    goback()
     print('已完成浏览会场任务')
     print('====== 浏览其他会场 ======')
     for i in range(1, 4):
-        os.system('.\\adb.exe shell input tap 900 1650')   # 点击去浏览1
+        os.system('.\\adb.exe shell input tap 900 1600')   # 点击去浏览1
         time.sleep(2)   # 滑动前休息2秒，避免网络不好
         initlocal()
         print('进入会场，浏览中，请等待 16 秒')
@@ -73,6 +73,6 @@ task1()
 task2()
 os.system('.\\adb.exe shell input swipe 900 1600 900 1200')    # 下拉任务列表
 time.sleep(1)
-task3()
+shopping()
 print('全部任务已完成')
 os.system("pause")
