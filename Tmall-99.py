2e8e0aa3c7b1e51866c61dbb0242f2e3bc2dc478
wukaibin <842387850@qq.com>
2019-10-31 09:21:53 +0800

    python文件

--- Tmall-99.py
+++ Tmall-99.py
@@ -25,8 +25,8 @@ def shopping():
         print('===== 逛*****店({}/18) ======'.format(i))
         os.system('.\\adb.exe shell input tap 900 1620')   # 进入逛店
         initlocal()
-        print('进入店铺，浏览中，请等待 16 秒')
-        time.sleep(16)
+        print('进入店铺，浏览中，请等待 20 秒')
+        time.sleep(20)
         goback()
         time.sleep(1)
     print('已完成逛店任务')
@@ -35,8 +35,8 @@ def task1():
     print('====== 浏览主会场 ======')
     os.system('.\\adb.exe shell input tap 900 1150')   # 点击去浏览
     initlocal()
-    print('进入主会场，浏览中，请等待 16 秒')
-    time.sleep(16)
+    print('进入主会场，浏览中，请等待 20 秒')
+    time.sleep(20)
     goback()
     print('已完成浏览主会场任务')
     print('====== 浏览其他会场 ======')
@@ -44,27 +44,27 @@ def task1():
         os.system('.\\adb.exe shell input tap 900 1300')   # 点击去浏览1
         time.sleep(2)   # 滑动前休息2秒，避免网络不好
         initlocal()
-        print('进入会场，浏览中，请等待 16 秒')
-        time.sleep(16)
+        print('进入会场，浏览中，请等待 20 秒')
+        time.sleep(20)
         goback()
 
 def task2():
     print('====== 浏览会场 ======')
-    # for i in range(1, 4):
-    os.system('.\\adb.exe shell input tap 900 1500')   # 点击去浏览
-    time.sleep(2)   # 滑动前休息2秒，避免网络不好
-    initlocal()
-    print('进入会场，浏览中，请等待 16 秒')
-    time.sleep(16)
-    goback()
+    for i in range(1, 4):
+        os.system('.\\adb.exe shell input tap 900 1500')   # 点击去浏览
+        time.sleep(2)   # 滑动前休息2秒，避免网络不好
+        initlocal()
+        print('进入会场，浏览中，请等待 20 秒')
+        time.sleep(20)
+        goback()
     print('已完成浏览会场任务')
     print('====== 浏览其他会场 ======')
     for i in range(1, 4):
         os.system('.\\adb.exe shell input tap 900 1600')   # 点击去浏览1
         time.sleep(2)   # 滑动前休息2秒，避免网络不好
         initlocal()
-        print('进入会场，浏览中，请等待 16 秒')
-        time.sleep(16)
+        print('进入会场，浏览中，请等待 20 秒')
+        time.sleep(20)
         goback()
  
 os.system('.\\adb.exe shell input tap 900 1700')   # 点击领喵币，弹出任务菜单
